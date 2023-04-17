13f88a53ac2538b753779feb45c1f4f3cbef03ff
Willian Hideak <whideak@gmail.com>
2023-04-17 20:53:34 -0300

    feat: add page to edit user

---
+++ web/components/page-login-manage-user/PageLoginManageUserForm.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const router = useRouter();
+</script>
+
+<template>
+    <BaseButton class="mb-2 w-100" type="button" color="dark" label="Alterar nome" />
+    <BaseButton class="mb-2 w-100" type="button" color="dark" label="Alterar e-mail" />
+    <BaseButton class="mb-2 w-100" type="button" color="dark" label="Alterar senha" />
+    <BaseButton class="w-100" type="button" color="dark" label="Desabilitar senha" />
+    <hr>
+    <BaseButton class="mb-2 w-100" type="button" color="danger" label="Excluir usuário" />
+    <BaseButton class="w-100" type="button" color="dark" label="Cancelar" @click="router.push('/login')" />
+</template>
---
+++ web/components/page-login-manage-user/PageLoginManageUserTitle.vue
@@ -0,0 +1,4 @@
+<template>
+    <h2>Willian Hideak</h2>
+    <p>O que você quer editar neste usuário?</p>
+</template>

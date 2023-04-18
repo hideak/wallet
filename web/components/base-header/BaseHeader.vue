4b9e59645c7105093000c9ed513bce7d09708c61
Willian Hideak <whideak@gmail.com>
2023-04-17 21:38:48 -0300

    feat: modify header to use back button

--- web/components/base-header/BaseHeader.vue
+++ web/components/base-header/BaseHeader.vue
@@ -1,5 +1,6 @@
 <template>
     <BaseHeaderContainer>
+        <BaseHeaderBackButton />
         <BaseHeaderBrand />
         <BaseHeaderToggle />
     </BaseHeaderContainer>
---
+++ web/components/base-header/BaseHeaderBackButton.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const router = useRouter();
+</script>
+
+<template>
+    <BaseButton class="text-white" icon="arrow-left" @click="router.back()" />
+</template>

735f642b3797a7f63a6d550e23a0d86755d0bddb
Willian Hideak <whideak@gmail.com>
2023-04-23 19:37:08 -0300

    feat: add password service

---
+++ web/services/password.service.ts
@@ -0,0 +1,15 @@
+import bcrypt from "bcrypt";
+
+export class PasswordService {
+    static saltRounds = 10;
+
+    async hash(password: string): Promise<string> {
+        return await bcrypt.hash(password, PasswordService.saltRounds);
+    }
+
+    async compare(password: string, hash: string): Promise<boolean> {
+        return await bcrypt.compare(password, hash);
+    }
+}
+
+export const passwordService = new PasswordService();

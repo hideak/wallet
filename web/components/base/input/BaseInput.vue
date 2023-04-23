e52a47453a88cd2b20a0b5a3c8adfb37a1389fcb
Willian Hideak <whideak@gmail.com>
2023-04-23 19:51:39 -0300

    feat: add two way data binding into input

--- web/components/base/input/BaseInput.vue
+++ web/components/base/input/BaseInput.vue
@@ -10,10 +10,13 @@ interface Props {
   buttonIcon?: string;
   buttonColor?: string;
   placeholder?: string;
+  modelValue: string;
   type: string;
 }
 
 defineProps<Props>();
+
+const emit = defineEmits(['update:modelValue']);
 </script>
 
 <template>
@@ -21,7 +24,8 @@ defineProps<Props>();
     <BaseIconLabel v-if="(upperIcon || upperLabel)" class="d-block mb-2" :label="upperLabel" :icon="upperIcon" />
     <BaseInputGroup>
       <BaseIconLabel v-if="(leftLabel || leftIcon)" class="input-group-text" :icon="leftIcon" :label="leftLabel" />
-      <input class="form-control" :type="type" :placeholder="placeholder">
+      <input class="form-control" :type="type" :placeholder="placeholder" :value="modelValue"
+        @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)">
       <BaseIconLabel v-if="(rightLabel || rightIcon)" class="input-group-text" :icon="rightIcon" :label="rightLabel" />
       <BaseButton v-if="(buttonIcon || buttonLabel)" :icon="buttonIcon" :label="buttonLabel" :color="buttonColor" />
     </BaseInputGroup>

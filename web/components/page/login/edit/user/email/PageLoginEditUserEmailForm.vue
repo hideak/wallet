d08321e30ef69e61d3c321b75aacfb0022477ee5
Willian Hideak <whideak@gmail.com>
2023-04-29 18:03:55 -0300

    feat: add user edit email form on page

--- web/components/page/login/edit/user/email/PageLoginEditUserEmailForm.vue
+++ web/components/page/login/edit/user/email/PageLoginEditUserEmailForm.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
+import BaseForm from '~/components/base/form/BaseForm.vue';
+import { UserEditEmailData } from '~/models/user/data/user-edit-email-data';
+
 const router = useRouter();
+
+const userEditEmailForm = ref<InstanceType<typeof BaseForm> | null>(null);
+const userEditEmailData = ref<UserEditEmailData | null>(null);
 </script>
 
 <template>
-    <BaseInput type="text" upperIcon="envelope" upperLabel="E-mail" />
-    <hr>
-    <BaseButton class="mb-2 w-100" type="button" color="primary" label="Salvar alterações" />
-    <BaseButton class="w-100" type="button" color="dark" label="Cancelar" @click="router.back()" />
+    <div>
+        <BaseSpinner v-if="!userEditEmailData" />
+        <BaseForm v-else ref="userEditEmailForm">
+            <BaseInput type="email" upperIcon="envelope" upperLabel="E-mail" v-model="userEditEmailData.email" required
+                invalidFeedback="Insira um e-mail válido." />
+            <hr>
+            <BaseButton class="mb-2 w-100" type="button" color="primary" label="Salvar alterações" />
+            <BaseButton class="w-100" type="button" color="dark" label="Cancelar" @click="router.back()" />
+        </BaseForm>
+    </div>
 </template>

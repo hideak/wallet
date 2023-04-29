865f8703982e645d0f8b41a75d7d3904692af55f
Willian Hideak <whideak@gmail.com>
2023-04-29 15:10:40 -0300

    feat: complete edit email page

--- web/components/page/login/edit/user/email/PageLoginEditUserEmailForm.vue
+++ web/components/page/login/edit/user/email/PageLoginEditUserEmailForm.vue
@@ -1,10 +1,38 @@
 <script setup lang="ts">
+import BaseForm from '~/components/base/form/BaseForm.vue';
+import { UserEditEmailData } from '~/models/user/data/user-edit-email-data';
+import { userService } from '~/services/user-service';
+
+const route = useRoute();
 const router = useRouter();
+const userEditEmailForm = ref<InstanceType<typeof BaseForm> | null>(null);
+const userEditEmailData = ref<UserEditEmailData | null>(null);
+
+onMounted(async () => {
+    const userId = Number(route.params.id);
+    userEditEmailData.value = await userService.getUserEditEmailData(userId);
+});
+
+const editUserEmail = async () => {
+    if (!userEditEmailData.value) { return; }
+    if (!userEditEmailForm.value?.validate()) { return; }
+
+    const userId = Number(route.params.id);
+    await userService.editUserEmail(userId, userEditEmailData.value);
+    router.back();
+};
 </script>
 
 <template>
-    <BaseInput type="text" upperIcon="envelope" upperLabel="E-mail" />
-    <hr>
-    <BaseButton class="mb-2 w-100" type="button" color="primary" label="Salvar alterações" />
-    <BaseButton class="w-100" type="button" color="dark" label="Cancelar" @click="router.back()" />
+    <div>
+        <BaseSpinner v-if="!userEditEmailData" />
+        <BaseForm v-else ref="userEditEmailForm">
+            <BaseInput type="email" upperIcon="envelope" upperLabel="E-mail" v-model="userEditEmailData.email" required
+                invalidFeedback="Insira um e-mail válido." />
+            <hr>
+            <BaseButton class="mb-2 w-100" type="button" color="primary" label="Salvar alterações"
+                @click="editUserEmail()" />
+            <BaseButton class="w-100" type="button" color="dark" label="Cancelar" @click="router.back()" />
+        </BaseForm>
+    </div>
 </template>
